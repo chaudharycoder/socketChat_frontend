5249561b1e4fa573061c2cce965f447f6d0299cf
Ankit kumar <ankitkumar.cse2026@gmail.com>
2026-01-10 00:34:30 +0530

    socketSlice change

--- src/store/slice/socket/socketSlice.js
+++ src/store/slice/socket/socketSlice.js
@@ -13,7 +13,7 @@ export const socketSlice = createSlice({
     initializeSocket: (state, action) => {
 
       const isDev = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-      const SOCKET_URL = isDev ? "http://localhost:5000" : "https://socketchat-lnrb.onrender.com";
+      const SOCKET_URL = isDev ? "http://localhost:5000" : "https://socketchat-backend-1.onrender.com";
 
       const socket = io(SOCKET_URL, {
         query: {
